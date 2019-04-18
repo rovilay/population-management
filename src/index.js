6617943ac6ceae1abd9bf73b1d5878ed7cd36aa3
rovilay <rovilayjnr01@gmail.com>
2019-04-18 07:37:37 +0100

    ch-setup-DB
    * setup mongo DB

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 
 import routes from './routes';
+import runDB from './config/db';
 
 dotenv.config();
 
 const { PORT = 4444 } = process.env;
 
 const app = express();
+runDB(); // initialize database connection
 
 app.use(cors());
 app.use(bodyParser.json());
